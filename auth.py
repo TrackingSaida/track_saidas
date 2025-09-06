5177b55bb21d9ba31a57e6ab39b00d8725dab910
mapadevsports <mapa.dev.sports@gmail.com>
2025-09-06 13:48:44 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -12,7 +12,7 @@ from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 
 from db import get_db
-from users_routes import User
+from users_routes_updated import User
 
 # Configurações JWT
 SECRET_KEY = os.getenv("SECRET_KEY", "sua-chave-secreta-super-segura-aqui-mude-em-producao")
