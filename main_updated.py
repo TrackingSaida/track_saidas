ddadc01649f186c88b9b6c5847cf67a9e59f1bf1
mapadevsports <mapa.dev.sports@gmail.com>
2025-09-11 11:44:38 -0300

    Update main_updated.py

--- main_updated.py
+++ main_updated.py
@@ -18,6 +18,8 @@ else:
         "https://track-saidas-html.onrender.com",
         "http://localhost:5500", "http://127.0.0.1:5500",
         "http://localhost:8000", "http://127.0.0.1:8000",
+        "http://localhost:3000", "http://172.30.33.97:3000",
+        
     ]
 
 # ──────────────────────────────────────────────────────────────────
