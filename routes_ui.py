3db156c45e5af291acafb011b03ced9c61531b75
acsilva065 <acsilva065@gmail.com>
2026-02-05 21:01:49 -0300

    icone etq

--- routes_ui.py
+++ routes_ui.py
@@ -42,7 +42,8 @@ MENU_DEFS = [
                 "label": "Gerar Etiqueta",
                 "href": "tracking-etiquetas.html",
                 "roles": [0, 1, 2, 3],
-                "group": "operacao_direto"
+                "group": "operacao_direto",
+                "icon": "ri-qr-code-line"
             },
         ]
     },
